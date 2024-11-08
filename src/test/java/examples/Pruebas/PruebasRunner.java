e3c3eb187d482ad29d81980743f91081cc685bc5
Axel Yataco <axelyataco1998@gmail.com>
2024-11-08 12:59:50 -0500

    Creation of PruebasRunner

---
+++ src/test/java/examples/Pruebas/PruebasRunner.java
@@ -0,0 +1,10 @@
+package examples.Pruebas;
+
+import com.intuit.karate.junit5.Karate;
+
+public class PruebasRunner {
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("prueba").relativeTo(getClass());
+    }
+}
